3a0cad021342cd7ce26d118c9ce159f7f6904f09
DerBot7214 <lengwenath1@gmail.com>
2024-09-26 18:10:35 +0200

    allow multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use copypasta::{ClipboardContext, ClipboardProvider};
-use indicatif::ProgressBar;
+use indicatif::{MultiProgress, ProgressBar};
 use is_terminal::IsTerminal;
 use serde::Deserialize;
 use ssh::{Session, RECURSIVE, WRITE};
@@ -21,7 +21,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stdin = stdin.lock();
 
     if args().len() == 2 && args().nth(1).unwrap() == "--help" {
-        println!("Usage: rs-scp <file_to_upload/file_name (only if file is piped)>");
+        println!("Usage: rs-scp <files_to_upload/file_name (only if file is piped)>");
         println!("Arguments:");
         println!("\t--help: Display this help message.");
         println!("\t--list: List all the files on the server.");
@@ -57,133 +57,151 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     session.connect().unwrap();
     session.userauth_publickey_auto(None).unwrap();
 
-    let file_name: &str;
-    let arg1 = &args[1].to_owned();
+    let multi = MultiProgress::new();
 
-    if arg1 == "--list" {
-        let path = config.path.as_str();
-
-        let mut channel = session.channel_new().unwrap();
-        channel.open_session().unwrap();
-        channel.request_exec(format!("ls -la {}", path).as_bytes()).unwrap();
-        let mut buffer = [0; 1024];
-        let mut data = String::new();
-        while channel.stdout().read(&mut buffer).unwrap() > 0 {
-            data.push_str(std::str::from_utf8(&buffer).unwrap());
+    for arg in &args {
+        if arg == &args[0] {
+            continue;
         }
-        channel.close();
-        println!("{}", data);
-        return Ok(());
-    } else if arg1.starts_with("--remove") {
-        let path = config.path.strip_suffix("/").unwrap();
 
-        let file_name = args[2].to_owned();
-        let mut channel = session.channel_new().unwrap();
-        channel.open_session().unwrap();
-        channel.request_exec(format!("rm  {}/{}", path, file_name).as_bytes()).unwrap();
-        let mut buffer = [0; 4096];
-        let mut data = String::new();
-        while channel.stderr().read(&mut buffer).unwrap() > 0 {
-            data.push_str(std::str::from_utf8(&buffer).unwrap());
-        }
-        channel.close();
-        if data.is_empty() {
-            println!("File removed successfully!");
-        } else {
+        let file_name: &str;
+        let arg1 = &arg.to_owned();
+
+        if arg1 == "--list" {
+            let path = config.path.as_str();
+
+            let mut channel = session.channel_new().unwrap();
+            channel.open_session().unwrap();
+            channel.request_exec(format!("ls -la {}", path).as_bytes()).unwrap();
+            let mut buffer = [0; 1024];
+            let mut data = String::new();
+            while channel.stdout().read(&mut buffer).unwrap() > 0 {
+                data.push_str(std::str::from_utf8(&buffer).unwrap());
+            }
+            channel.close();
             println!("{}", data);
-        }
-        return Ok(());
-    }
+            return Ok(());
+        } else if arg1.starts_with("--remove") {
+            let path = config.path.strip_suffix("/").unwrap();
 
-    const CHUNK_SIZE: usize = 1024;
-    if atty::is(atty::Stream::Stdin) {
-        let file_path = arg1;
-        let mut file = File::open(file_path)?;
-        let file_size = file.metadata()?.len() as usize;
-
-        file_name = Path::new(file_path).file_name().unwrap().to_str().unwrap();
-
-        let mut scp = session.scp_new(RECURSIVE | WRITE, config.path.as_str()).unwrap();
-        scp.init().unwrap();
-        scp.push_file(Path::new(file_name), file_size, 0o644).unwrap();
-
-        let mut buffer = vec![0; CHUNK_SIZE];
-
-        let pb = ProgressBar::new(file_size as u64);
-        pb.set_style(indicatif::ProgressStyle::default_bar()
-            .template("Uploading: {msg} ({total_bytes})\n{bytes}  {binary_bytes_per_sec} {elapsed_precise} [{wide_bar}] {percent_precise}%").unwrap()
-            .progress_chars("#- ")
-        );
-        pb.set_message(file_name.to_string());
-
-        loop {
-            let bytes_read = file.read(&mut buffer).unwrap();
-            if bytes_read == 0 {
-                break;
+            let file_name = args[2].to_owned();
+            let mut channel = session.channel_new().unwrap();
+            channel.open_session().unwrap();
+            channel.request_exec(format!("rm  {}/{}", path, file_name).as_bytes()).unwrap();
+            let mut buffer = [0; 4096];
+            let mut data = String::new();
+            while channel.stderr().read(&mut buffer).unwrap() > 0 {
+                data.push_str(std::str::from_utf8(&buffer).unwrap());
             }
-
-            scp.write(&buffer[..bytes_read]).unwrap();
-            pb.inc(bytes_read as u64);
+            channel.close();
+            if data.is_empty() {
+                println!("File removed successfully!");
+            } else {
+                println!("{}", data);
+            }
+            return Ok(());
         }
 
-        pb.finish_and_clear();
-        scp.flush().unwrap();
-        scp.close();
-    } else {
-        file_name = arg1;
+        const CHUNK_SIZE: usize = 1024;
+        if atty::is(atty::Stream::Stdin) {
+            let file_path = arg1;
+            let mut file = File::open(file_path)?;
+            let file_size = file.metadata()?.len() as usize;
 
-        let mut buffer = Vec::new();
-        stdin.read_to_end(&mut buffer).unwrap();
-        let file_size = buffer.len();
+            file_name = Path::new(file_path).file_name().unwrap().to_str().unwrap();
 
-        let mut scp = session.scp_new(RECURSIVE | WRITE, config.path.as_str()).unwrap();
-        scp.init().unwrap();
-        scp.push_file(Path::new(file_name), file_size, 0o644).unwrap();
+            let mut scp = session.scp_new(RECURSIVE | WRITE, config.path.as_str()).unwrap();
+            scp.init().unwrap();
+            scp.push_file(Path::new(file_name), file_size, 0o644).unwrap();
 
-        scp.write(&buffer).unwrap();
+            let mut buffer = vec![0; CHUNK_SIZE];
 
-        scp.flush().unwrap();
-        scp.close();
-    }
+            let progress_line = "{binary_bytes_per_sec} {elapsed_precise} [{bar:40}] {percent_precise}%";
 
-    let ctx = ClipboardContext::new();
+            let pb = ProgressBar::new(file_size as u64);
+            pb.set_style(indicatif::ProgressStyle::default_bar()
+                .template(&format!("Uploading: {{msg}} ({{total_bytes}})\n{}", format!("{{bytes}} {}", progress_line))).unwrap()
+                .progress_chars("#- ")
+            );
+            pb.set_message(file_name.to_string());
 
-    let url = match config.host.strip_suffix("/") {
-        Some(host) => format!("https://{}/{}", host, file_name),
-        None => format!("https://{}/{}", config.host, file_name),
-    };
+            loop {
+                let bytes_read = file.read(&mut buffer).unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
 
-    if std::io::stdout().is_terminal() {
-        println!("File uploaded successfully! URL: {}", url);
-
-        let mut use_clipboard = false;
-
-        if env::var("WAYLAND_DISPLAY").is_ok() {
-            let mut child = Command::new("wl-copy")
-                .stdin(Stdio::piped())
-                .spawn()
-                .expect("Failed to copy URL to clipboard");
-
-            let stdin = child.stdin.as_mut().unwrap();
-            stdin.write_all(url.as_bytes()).unwrap();
-
-            let output = child.wait_with_output().unwrap();
-            if !output.status.success() {
-                use_clipboard = true;
+                scp.write(&buffer[..bytes_read]).unwrap();
+                pb.inc(bytes_read as u64);
             }
-        } else if env::var("DISPLAY").is_ok() {
-            use_clipboard = true;
+
+            pb.set_style(indicatif::ProgressStyle::default_bar()
+                .template(&format!("{{wide_msg}} {}", progress_line)).unwrap()
+                .progress_chars("#- ")
+            );
+            pb.set_position(file_size as u64);
+            pb.finish();
+            multi.add(pb);
+            scp.flush().unwrap();
+            scp.close();
         } else {
-            use_clipboard = true;
+            file_name = arg1;
+
+            let mut buffer = Vec::new();
+            stdin.read_to_end(&mut buffer).unwrap();
+            let file_size = buffer.len();
+
+            let mut scp = session.scp_new(RECURSIVE | WRITE, config.path.as_str()).unwrap();
+            scp.init().unwrap();
+            scp.push_file(Path::new(file_name), file_size, 0o644).unwrap();
+
+            scp.write(&buffer).unwrap();
+
+            scp.flush().unwrap();
+            scp.close();
         }
 
-        if use_clipboard && !ctx.is_err() {
-            ctx.unwrap().set_contents(url.to_owned()).unwrap();
-        }
+        let ctx = ClipboardContext::new();
 
-        println!("URL copied to clipboard!");
-    } else {
-        print!("{}", url);
+        let url = match config.host.strip_suffix("/") {
+            Some(host) => format!("https://{}/{}", host, file_name),
+            None => format!("https://{}/{}", config.host, file_name),
+        };
+
+        if args.len() < 3 {
+            if std::io::stdout().is_terminal() {
+                println!("File uploaded successfully! URL: {}", url);
+
+                let mut use_clipboard = false;
+
+                if env::var("WAYLAND_DISPLAY").is_ok() {
+                    let mut child = Command::new("wl-copy")
+                        .stdin(Stdio::piped())
+                        .spawn()
+                        .expect("Failed to copy URL to clipboard");
+
+                    let stdin = child.stdin.as_mut().unwrap();
+                    stdin.write_all(url.as_bytes()).unwrap();
+
+                    let output = child.wait_with_output().unwrap();
+                    if !output.status.success() {
+                        use_clipboard = true;
+                    }
+                } else if env::var("DISPLAY").is_ok() {
+                    use_clipboard = true;
+                } else {
+                    use_clipboard = true;
+                }
+
+                if use_clipboard && !ctx.is_err() {
+                    ctx.unwrap().set_contents(url.to_owned()).unwrap();
+                }
+
+                println!("URL copied to clipboard!");
+            } else {
+                print!("{}", url);
+            }
+        }
     }
 
     Ok(())
